90e698d1622357170424180dcac684248da84782
Simon Leighton <swleighton@live.co.uk>
2019-05-22 15:02:20 +1000

    Extended UI to allow filtering by params

--- product-api/Resources/Scripts/scripts.js
+++ product-api/Resources/Scripts/scripts.js
@@ -7,15 +7,26 @@ document.getElementById('req-form').addEventListener("submit", function (e) {
 
         var request = "/api/products";
         var queryFields = document.getElementById('req-form').getElementsByTagName("input");
+        var queryParams = "";
 
         if(queryFields){
             for (field of queryFields) {
                 fieldValue = field.value;
                 if (field.id == "Id" && fieldValue != "") {
                     request+= `/${fieldValue}`
+                    break; //if searching on ID ignore other params
+                } else if (fieldValue != "") {
+                    fieldQueryParam = `${field.id}=${fieldValue}`
+                    if (queryParams == "") {
+                        queryParams = `?${fieldQueryParam}`;  
+                    } else {
+                        queryParams += `&${fieldQueryParam}`; 
+                    }
                 }
             }
+            request += queryParams;
         }
+
 
         document.getElementById('req-data').classList.remove('hide');
         document.getElementById('req-url').innerText = request;
