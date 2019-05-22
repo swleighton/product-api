326e2b12c3031f623655a754299227d27fb87d03
Simon Leighton <swleighton@live.co.uk>
2019-05-22 11:14:46 +1000

    Created base get products API UI

---
+++ product-api/Resources/Scripts/scripts.js
@@ -0,0 +1,18 @@
+﻿    document.getElementById('req-form').addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        var request = "/api/products";
+        document.getElementById('req-data').classList.remove('hide');
+        document.getElementById('req-url').innerText = request;
+        document.getElementById('req-body').innerHTML = "<i>Empty</i>";
+
+        fetch(request, {
+            method: 'get'
+        })
+            .then(function (response) {
+                response.json().then(function (data) {
+                    document.getElementById('res-status').innerText = `${response.status}  ${response.statusText}`;
+                    document.getElementById('res-body').innerHTML = JSON.stringify(data);
+            });
+    })
+    });
